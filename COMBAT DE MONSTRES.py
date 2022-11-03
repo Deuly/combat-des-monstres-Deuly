13155fce677bee6c5c0f1ada816d98d71ee7b311
dybkaspiesj <dybkaspiesj@educ.college-montreal.qc.ca>
2022-11-03 15:57:32 -0400

    initial commit

---
+++ COMBAT DE MONSTRES.py
@@ -0,0 +1,14 @@
+"""
+creer le : 2022-11-03
+creer par : Justin-Deuly Dybka-Spies
+type de jeu: jeu de monstre
+"""
+
+
+from random import randint
+jeu_debut = True
+while jeu_debut :
+    print("voici un jeu de monstre , au cour de ce jeu vous allez devoir combattre des monstres a l`aide de vos choix")
+
+
+
